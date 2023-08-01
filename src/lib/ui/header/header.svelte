d2b737729fcef8e6f713fd01241fd760be9b1539
Raymond Li <li.raymond04@gmail.com>
2023-07-31 22:14:03 -0700

    Fix close menu on outside click and close menu with hamburger button Fix header layout styling Add dummy pages for test routes

--- src/lib/ui/header/header.svelte
+++ src/lib/ui/header/header.svelte
@@ -1,41 +1,70 @@
 <script lang="ts">
-	import Items from "./items.svelte";
+	import Items from './items.svelte';
+	import { clickoutside } from '@svelte-put/clickoutside';
+
+	let clicked = false;
+
+	const close = (e: MouseEvent | KeyboardEvent) => {
+		const et = e.target as HTMLInputElement;
+		if (clicked && document.activeElement instanceof HTMLElement) {
+			if (et.localName == 'summary') return;
+			document.activeElement.blur();
+			clicked = false;
+		} else {
+			clicked = true;
+		}
+	};
 </script>
 
-<div class="navbar bg-base-100">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<div tabindex="0" class="btn btn-ghost sm:hidden" role="tab">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</div>
-			<ul
+<div class="m-2">
+	<div class="navbar bg-base-100 rounded-lg drop-shadow-lg">
+		<div class="grow-0 w-[50%]">
+			<div
+				class="dropdown"
 				tabindex="0"
-				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-				role="menu"
+				role="button"
+				on:click={close}
+				on:keypress={close}
+				use:clickoutside
+				on:clickoutside={() => {
+					clicked = false;
+				}}
 			>
+				<!-- hamburger menu icon -->
+				<div tabindex="0" class="btn btn-ghost pointer-events-none sm:hidden" role="tab">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/></svg
+					>
+				</div>
+				<ul
+					tabindex="0"
+					class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+					role="menu"
+				>
+					<Items />
+				</ul>
+			</div>
+			<a class="btn btn-ghost normal-case text-xl" href="/">liraymond04</a>
+		</div>
+		<div class="grow shrink-0 justify-center hidden sm:flex">
+			<ul class="menu menu-horizontal px-1" role="menu">
 				<Items />
 			</ul>
 		</div>
-		<a class="btn btn-ghost normal-case text-xl" href="/">liraymond04</a>
-	</div>
-	<div class="navbar-center hidden sm:flex">
-		<ul class="menu menu-horizontal px-1">
-			<Items />
-		</ul>
-	</div>
-	<div class="navbar-end">
-		<a class="btn" href="/about">About</a>
+		<!-- divider to force last component to rightmost end -->
+		<div class="sm:hidden grow" />
+		<div class="grow-0 w-[50%] justify-end">
+			<a class="btn" href="/about">About</a>
+		</div>
 	</div>
 </div>
--- src/lib/ui/header/items.svelte
+++ src/lib/ui/header/items.svelte
@@ -1,13 +1,28 @@
-<li><a href="/">Item 1</a></li>
+<script lang="ts">
+	import { clickoutside } from '@svelte-put/clickoutside';
+
+	const close = (e: CustomEvent<MouseEvent>) => {
+		const et = e.target as HTMLInputElement;
+		if (et.hasAttribute('open')) et.removeAttribute('open');
+	};
+
+	const closelink = (e: MouseEvent) => {
+		const et = e.target as HTMLInputElement;
+		const menu = et.parentElement?.parentElement?.parentElement;
+		if (menu) menu.removeAttribute('open');
+	};
+</script>
+
+<li><a href="/projects">Projects</a></li>
+
+<li><a href="/resume">Resume</a></li>
 
 <li tabindex="0" role="tab">
-	<details>
-		<summary>Parent</summary>
+	<details use:clickoutside on:clickoutside={close}>
+		<summary>Articles</summary>
 		<ul class="p-2">
-			<li><a href="/">Submenu 1</a></li>
-			<li><a href="/">Submenu 2</a></li>
+			<li><a href="/articles/technical" on:click={closelink}>Technical</a></li>
+			<li><a href="/articles/personal" on:click={closelink}>Personal</a></li>
 		</ul>
 	</details>
 </li>
-	
-<li><a href="/">Item 3</a></li>
