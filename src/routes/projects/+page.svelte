fb41f114da267877d8ff3f863c83eedbc3bbfb50
Raymond Li <li.raymond04@gmail.com>
2023-08-28 10:45:30 -0700

    Add fuse.js for fuzzy searching of page items

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import Fuse from 'fuse.js'
+
 	export let data;
-	$: items = data.props.items;
+	
+	const fuse = new Fuse(data.props.items, {
+		keys: ['path', 'tags', 'keywords']
+	})
 
 	let search = '';
+	$: results = fuse.search(search).map(i => i.item)
+
+	$: items = search ? results : data.props.items
 </script>
 
 <h1>Projects</h1>
@@ -10,10 +18,7 @@
 <input bind:value={search} type="text" class="mx-4 mt-4" placeholder="Search..." />
 
 <div class="m-4">
-	{#each items.filter((item) => {
-		if (search === '') return true;
-		return item?.keywords?.includes(search);
-	}) as item}
+	{#each items as item}
 		<a class="flex flex-col hover:cursor-pointer hover:underline" href={`/projects/${item.path}`}
 			>{item.path}</a
 		>
