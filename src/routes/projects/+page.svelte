2b94f9229be8a04dc042ac6b58feadaf4918f089
Raymond Li <li.raymond04@gmail.com>
2023-08-31 22:51:55 -0700

    Add multi-search filters (only intersect for now)

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,46 +1,141 @@
 <script lang="ts">
+	import type { SearchItem } from '$lib/ui/sidebar/types.js';
 	import Fuse from 'fuse.js';
+	import 'iconify-icon';
 
 	export let data;
 
-	let path = true,
-		tags = true,
-		keywords = true;
+	interface Search {
+		val: string;
+		path: boolean;
+		tags: boolean;
+		keywords: boolean;
+		function: 'and' | 'or';
+		fuse: Fuse<SearchItem>;
+	}
 
-	$: fuse = new Fuse(data.props.items, {
-		keys: [...(path ? ['path'] : []), ...(tags ? ['tags'] : []), ...(keywords ? ['keywords'] : [])]
-	});
+	let additional: Search[] = [
+		{
+			val: '',
+			path: true,
+			tags: true,
+			keywords: true,
+			function: 'and',
+			fuse: new Fuse(data.props.items, {
+				keys: ['path', 'tags', 'keywords']
+			})
+		}
+	];
 
-	let search = '';
-	$: results = fuse.search(search).map((i) => i.item);
+	function intersect<T>(a: T[], b: T[]) {
+		return a.filter(Set.prototype.has, new Set(b));
+	}
 
-	$: items = search ? results : data.props.items;
+	function union<T>(a: T[], b: T[]) {
+		return [...new Set([...a, ...b])];
+	}
+
+	function resolve(results: SearchItem[][]): SearchItem[] {
+		let result = results[0];
+
+		for (let i = 0; i < results.length; i++) {
+			if (additional[i].val === '') continue;
+			if (additional[i].function === 'and') {
+				result = intersect(result, results[i]);
+			} else if (additional[i].function === 'or') {
+				result = union(result, results[i]);
+			}
+		}
+
+		return result;
+	}
+
+	$: results = additional.map((item) => item.fuse.search(item.val).map((i) => i.item));
+
+	$: items =
+		additional.reduce((total, i) => total + (i.val !== '' ? 1 : 0), 0) !== 0
+			? resolve(results)
+			: data.props.items;
 </script>
 
 <h1>Projects</h1>
 
-<div class="flex flex-wrap items-center space-x-2 mx-4 mt-4">
-	<input bind:value={search} type="text" placeholder="Search..." class="h-8" />
-	<div class="py-4">
-		<button
-			on:click={() => {
-				path = !path;
-			}}
-			class={`p-1 h-auto rounded ${path && 'bg-gray-700'}`}>path</button
-		>
-		<button
-			on:click={() => {
-				tags = !tags;
-			}}
-			class={`p-1 h-auto rounded ${tags && 'bg-gray-700'}`}>tags</button
-		>
-		<button
-			on:click={() => {
-				keywords = !keywords;
-			}}
-			class={`p-1 h-auto rounded ${keywords && 'bg-gray-700'}`}>keywords</button
-		>
-	</div>
+<div class="flex flex-col space-y-2 mx-4 mt-4">
+	{#each additional as search, index}
+		<div class="flex flex-wrap items-center space-x-2">
+			<input bind:value={search.val} type="text" placeholder="Search..." class="h-8" />
+			<div class="py-2">
+				<button
+					on:click={() => {
+						search.path = !search.path;
+						search.fuse = new Fuse(data.props.items, {
+							keys: [
+								...(search.path ? ['path'] : []),
+								...(search.tags ? ['tags'] : []),
+								...(search.keywords ? ['keywords'] : [])
+							]
+						});
+					}}
+					class={`p-1 h-auto rounded ${search.path && 'bg-gray-700'}`}>path</button
+				>
+				<button
+					on:click={() => {
+						search.tags = !search.tags;
+						search.fuse = new Fuse(data.props.items, {
+							keys: [
+								...(search.path ? ['path'] : []),
+								...(search.tags ? ['tags'] : []),
+								...(search.keywords ? ['keywords'] : [])
+							]
+						});
+					}}
+					class={`p-1 h-auto rounded ${search.tags && 'bg-gray-700'}`}>tags</button
+				>
+				<button
+					on:click={() => {
+						search.keywords = !search.keywords;
+						search.fuse = new Fuse(data.props.items, {
+							keys: [
+								...(search.path ? ['path'] : []),
+								...(search.tags ? ['tags'] : []),
+								...(search.keywords ? ['keywords'] : [])
+							]
+						});
+					}}
+					class={`p-1 h-auto rounded ${search.keywords && 'bg-gray-700'}`}>keywords</button
+				>
+			</div>
+			<button
+				class="flex content-center hover:cursor-pointer"
+				on:click={() => {
+					additional.push({
+						val: '',
+						path: true,
+						tags: true,
+						keywords: true,
+						function: 'and',
+						fuse: new Fuse(data.props.items, {
+							keys: ['path', 'tags', 'keywords']
+						})
+					});
+					additional = additional;
+				}}
+			>
+				<iconify-icon icon="ic:baseline-add-circle-outline" class="text-xl" />
+			</button>
+			{#if index !== 0}
+				<button
+					class="flex content-center hover:cursor-pointer"
+					on:click={() => {
+						additional.splice(index, 1);
+						additional = additional;
+					}}
+				>
+					<iconify-icon icon="ic:baseline-remove-circle-outline" class="text-xl" />
+				</button>
+			{/if}
+		</div>
+	{/each}
 </div>
 
 <div class="flex flex-col space-y-2 m-4">
