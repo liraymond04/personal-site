f8ccaccf5b58879434cfd15dcd556961dd7f7717
Raymond Li <li.raymond04@gmail.com>
2024-06-19 21:44:26 -0600

    Remove comments from home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,17 +16,4 @@
 			<Social size={72} />
 		</div>
 	</div>
-	
-	<!-- <div class="hidden sm:flex shrink-0 w-[240px] my-2"> -->
-	<!-- 	<Sidebar root_dir="" root_item={data.props.root} /> -->
-	<!-- </div> -->
-	<!---->
-	<!-- <main class="sm:max-w-[calc(100vw-240px)] w-full"> -->
-	<!-- 	<div class="sm:hidden w-full"> -->
-	<!-- 		<Sidebar root_dir="" root_item={data.props.root} /> -->
-	<!-- 	</div> -->
-	<!-- 	<div class="mx-6 my-2"> -->
-	<!-- 		<slot /> -->
-	<!-- 	</div> -->
-	<!-- </main> -->
 </div>
