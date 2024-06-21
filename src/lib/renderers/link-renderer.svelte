10ba31554cd28de5d7c7276a4ae78f05dafec3e8
Raymond Li <li.raymond04@gmail.com>
2024-06-21 02:49:13 -0600

    Fix errors when checking if URL is relative

--- src/lib/renderers/link-renderer.svelte
+++ src/lib/renderers/link-renderer.svelte
@@ -4,35 +4,32 @@
 	export let href = '';
 	export let title: string | undefined = undefined;
 
-	function isExternalURL(url: string) {
-		// Create a URL object from the given URL string
-		const link = new URL(url, window.location.origin);
-
-		// Check if the hostname of the link is different from the current site's hostname
-		return link.hostname !== window.location.hostname;
+	function isRelativeUrl(url: string) {
+		// A URL is considered relative if it does not start with a scheme like 'http://' or 'https://'
+		return !/^(?:[a-z]+:)?\/\//i.test(url);
 	}
 
-	if (!isExternalURL(href) && href.endsWith('/index.md')) {
-		href = href.replace('/index.md', '')
+	if (isRelativeUrl(href) && href.endsWith('/index.md')) {
+		href = href.replace('/index.md', '');
 	}
 
-	if (!isExternalURL(href) && href.endsWith('.md')) {
-		href = '../' + href.replace('.md', '')
+	if (isRelativeUrl(href) && href.endsWith('.md')) {
+		href = '../' + href.replace('.md', '');
 	}
 
 	$: dir = ''
 
 	$: {
 		const { url } = $page;
-		dir = url.pathname
+		dir = url.pathname;
 	}
 </script>
 
 <a
-	href={isExternalURL(href) ? href : `${dir}/${href}`}
+	href={!isRelativeUrl(href) ? href : `${dir}/${href}`}
 	{title}
 	class="link-base visited:link-visit link-hover"
-	target={isExternalURL(href) ? '_blank' : ''}
+	target={!isRelativeUrl(href) ? '_blank' : ''}
 >
 	<slot />
 </a>
