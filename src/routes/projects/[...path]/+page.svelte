91ffc632d89092ac77f631f6ebb4274da7fce560
Raymond Li <li.raymond04@gmail.com>
2024-06-22 01:14:58 -0600

    Move custom markdown renderer to component

--- src/routes/projects/[...path]/+page.svelte
+++ src/routes/projects/[...path]/+page.svelte
@@ -1,38 +1,9 @@
 <script lang="ts">
-	import SvelteMarkdown from 'svelte-markdown';
-	
-	import HeadingRenderer from '$lib/renderers/heading-renderer.svelte';
-	import ParagraphRenderer from '$lib/renderers/paragraph-renderer.svelte';
-	import LinkRenderer from '$lib/renderers/link-renderer.svelte';
-	import ListRenderer from '$lib/renderers/list-renderer.svelte';
-	import ListitemRenderer from '$lib/renderers/listitem-renderer.svelte';
-	import TableRenderer from '$lib/renderers/table-renderer.svelte';
-	import TablebodyRenderer from '$lib/renderers/tablebody-renderer.svelte';
-	import TablecellRenderer from '$lib/renderers/tablecell-renderer.svelte';
-	import TableheadRenderer from '$lib/renderers/tablehead-renderer.svelte';
-	import TablerowRenderer from '$lib/renderers/tablerow-renderer.svelte';
-	import CodeRenderer from '$lib/renderers/code-renderer.svelte';
-	import CodespanRenderer from '$lib/renderers/codespan-renderer.svelte';
+	import Markdown from '$lib/ui/markdown/markdown.svelte';
 
 	export let data;
 
 	$: source = data.props.markdownContent;
 </script>
 
-<SvelteMarkdown
-	{source}
-	renderers={{
-		heading: HeadingRenderer,
-		paragraph: ParagraphRenderer,
-		link: LinkRenderer,
-		list: ListRenderer,
-		listitem: ListitemRenderer,
-		table: TableRenderer,
-		tablebody: TablebodyRenderer,
-		tablecell: TablecellRenderer,
-		tablehead: TableheadRenderer,
-		tablerow: TablerowRenderer,
-		code: CodeRenderer,
-		codespan: CodespanRenderer
-	}}
-/>
+<Markdown {source} />
