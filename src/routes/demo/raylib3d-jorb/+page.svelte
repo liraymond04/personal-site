608ed9c303951711bce22a34b3102d8604e9e9ba
Raymond Li <li.raymond04@gmail.com>
2024-08-15 10:50:32 -0600

    Add demo page for raylib 3d example app

---
+++ src/routes/demo/raylib3d-jorb/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let canvas: HTMLCanvasElement | undefined;
+	let scriptLoaded = false;
+
+	onMount(() => {
+		if (canvas) {
+			window.Module = { canvas: canvas };
+		}
+
+		if (!scriptLoaded) {
+			const script = document.createElement('script');
+			script.src = '/wasm/awa5_rs-wasm-raylib.js';
+			script.onload = () => {
+				scriptLoaded = true;
+				setTimeout(() => {
+					updateCanvasWidth();
+				}, 500);
+			};
+			document.head.appendChild(script);
+			window.addEventListener('resize', updateCanvasWidth);
+		}
+	});
+
+	function updateCanvasWidth() {
+		if (canvas) {
+			canvas.style.width = '80%';
+		}
+	}
+</script>
+
+<body>
+	<canvas class="canvas" bind:this={canvas} />
+</body>
+
+<style>
+	.canvas {
+		/* width: 100%; */
+		padding-right: 0;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+	}
+</style>
