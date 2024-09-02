0d4c12486800b9cad6b4277b1f7e513f9e908d62
Raymond Li <li.raymond04@gmail.com>
2024-09-02 00:16:05 -0600

    Add default error page
    (cherry picked from commit def270c6820c93de95df0b08dfe2282d9cc5d4fd)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page?.status || "Error"} | liraymond04</title>
+	<meta name="description" content={`${$page.error?.message || "Something went wrong"}`} />
+</svelte:head>
+
+<div class="p-8">
+	<div class="error-page">
+		<div class="status">{$page?.status}</div>
+		<div class="message">{$page.error?.message}</div>
+	</div>
+</div>
+
+<style>
+	.error-page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+
+	.status {
+		font-size: 6rem;
+		font-weight: bold;
+	}
+
+	.message {
+		font-size: 1.5rem;
+	}
+</style>
