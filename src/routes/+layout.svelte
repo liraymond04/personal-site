02af15ad2b1d31c5b8b019f0ec3ecea49466f010
Raymond Li <li.raymond04@gmail.com>
2024-09-01 22:28:59 -0600

    Add Google analytics
    (cherry picked from commit 0fb493036e05f217010f87150e394a3ac875eaba)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,22 @@
 	import Header from '$lib/ui/header/header.svelte';
 	import Footer from '$lib/ui/footer/footer.svelte';
 	import '../app.css';
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag() {
+			window.dataLayer.push(arguments);
+		};
+		window.gtag('js', new Date());
+
+		window.gtag('config', 'G-CVCZVRFDHJ');
+	}
 </script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CVCZVRFDHJ"></script>
+</svelte:head>
 
 <div class="flex flex-col h-screen">
 	<Header />
