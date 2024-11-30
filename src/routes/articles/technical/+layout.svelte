aa562e411a8a6ec2d3fdd3fad6102f5e808e8571
liraymond04 <li.raymond04@gmail.com>
2024-11-29 21:23:08 -0800

    Center article content to screen even with sidebar
    (cherry picked from commit 86b1b44fcd137f3146b6444265f46142912cb8a5)

--- src/routes/articles/technical/+layout.svelte
+++ src/routes/articles/technical/+layout.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
 	import Sidebar from '$lib/ui/sidebar/sidebar.svelte';
+	import { onMount } from 'svelte';
 
 	export let data;
+
+	let articleElement: HTMLElement;
+	$: articleWidth = 0;
+	$: screenWidth = window.innerWidth;
+
+	const updateScreenWidth = () => {
+		screenWidth = window.innerWidth;
+	};
+
+	onMount(() => {
+		const resizeObserver = new ResizeObserver(() => {
+			if (articleElement) {
+				articleWidth = articleElement.offsetWidth;
+			}
+		});
+
+		window.addEventListener('resize', updateScreenWidth);
+
+		if (articleElement) {
+			resizeObserver.observe(articleElement);
+		}
+
+		return () => {
+			resizeObserver.disconnect();
+			window.removeEventListener('resize', updateScreenWidth);
+		};
+	});
+
+	$: centeredArticleWidth = (screenWidth - articleWidth) / 2 - 340;
 </script>
 
 <div class="flex pb-12">
@@ -9,11 +39,11 @@
 		<Sidebar root_dir="" root_item={data.props.root} />
 	</div>
 
-	<main class="sm:max-w-[calc(100vw-340px)] w-full">
+	<main class="sm:max-w-[calc(100vw-340px)] w-full" style="padding-left: {centeredArticleWidth}px;">
 		<div class="sm:hidden w-full">
 			<Sidebar root_dir="" root_item={data.props.root} />
 		</div>
-		<div class="mx-6 my-2 max-w-4xl">
+		<div class="mx-6 my-2 max-w-4xl" bind:this={articleElement}>
 			<slot />
 		</div>
 	</main>
