f5622f5987903cc7918f3f37c10b3927fb699b74
liraymond04 <li.raymond04@gmail.com>
2024-11-30 18:57:04 -0800

    Fix trailing slash

--- src/routes/projects/[...path]/+page.server.ts
+++ src/routes/projects/[...path]/+page.server.ts
@@ -3,6 +3,8 @@ import { parseMetadata } from '$lib/metadata';
 import { getLatestCommitSha, getCommitInfoFromPath, getGithubDetailsFromMedata, decodeContentFromCommitInfo, IsGithubFileCommitInfo } from '$lib/github';
 import { loadRemoteImagePaths, loadRemoteIndex } from '$lib/remote';
 
+export const trailingSlash = 'never';
+
 const dir = '/static/projects'
 const files = import.meta.glob("/static/projects/**/*.md", { as: 'raw' });
 
@@ -74,6 +76,9 @@ const run: PageServerLoad = async ({ params, parent }) => {
 
   if (isFilePath(params_path)) {
     const { github_owner, github_repo, title } = metadata;
+    if (!github_owner || !github_repo || !title) {
+      return;
+    }
     const full_path = params_path.replace(`${title}/`, '')
     const file = await getCommitInfoFromPath(github_owner.toString(), github_repo.toString(), full_path);
 
