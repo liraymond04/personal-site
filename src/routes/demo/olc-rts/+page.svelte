e9cac637a1b59d8d97e36773b16391d1c24bf5cf
Raymond Li <li.raymond04@gmail.com>
2025-08-09 18:22:53 -0600

    Add demo page for olc-rts

---
+++ src/routes/demo/olc-rts/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Demo | olc-rts | liraymond04</title>
+	<meta name="description" content="Demo page for olc-rts WASM build" />
+</svelte:head>
+
+<div class="h-screen">
+	<iframe title="AWA5.0" width="100%" height="100%" src="/wasm/olc-rts/main.html"></iframe>
+</div>
